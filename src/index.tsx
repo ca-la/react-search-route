cffbcba6e3f88fb2103bdf22a9f0c962d8607fc7
alevkon <alevkon@gmail.com>
2020-05-22 18:26:20 +0300

    Upgrade prettier (#6)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import SearchRoute, {
   SearchParams,
   SearchRouteProps,
-  WithSearchMatch
-} from './search-route';
+  WithSearchMatch,
+} from "./search-route";
 
-export { default as matchSearch, SearchMatchParams } from './match-search';
-export { default as SearchSwitch } from './switch';
+export { default as matchSearch, SearchMatchParams } from "./match-search";
+export { default as SearchSwitch } from "./switch";
 export { SearchParams, SearchRouteProps, WithSearchMatch };
 export default SearchRoute;
--- src/match-search.spec.tsx
+++ src/match-search.spec.tsx
@@ -1,76 +1,76 @@
-import matchSearch from './match-search';
+import matchSearch from "./match-search";
 
-describe('matchSearch', () => {
-  it('should calculate the correct match for empty values', () => {
+describe("matchSearch", () => {
+  it("should calculate the correct match for empty values", () => {
     // A non-exact match against no query parameters
-    expect(matchSearch(undefined, undefined, false, '')).toEqual({
+    expect(matchSearch(undefined, undefined, false, "")).toEqual({
       isExact: true,
       search: {},
-      searchKeyset: []
+      searchKeyset: [],
     });
 
     // An exact match against no query parameters
-    expect(matchSearch(undefined, undefined, true, '')).toEqual({
+    expect(matchSearch(undefined, undefined, true, "")).toEqual({
       isExact: true,
       search: {},
-      searchKeyset: []
+      searchKeyset: [],
     });
 
     // An exact match against search string query parameters
     expect(
-      matchSearch(undefined, undefined, true, '?foo=bar&fizz=buzz')
+      matchSearch(undefined, undefined, true, "?foo=bar&fizz=buzz")
     ).toEqual(null);
 
     // A non-exact match against search string query parameters
     expect(
-      matchSearch(undefined, undefined, false, '?foo=bar&fizz=buzz')
+      matchSearch(undefined, undefined, false, "?foo=bar&fizz=buzz")
     ).toEqual({
       isExact: false,
       search: {
-        fizz: 'buzz',
-        foo: 'bar'
+        fizz: "buzz",
+        foo: "bar",
       },
-      searchKeyset: []
+      searchKeyset: [],
     });
   });
 
-  it('should calculate the correct match against the supplied search object', () => {
+  it("should calculate the correct match against the supplied search object", () => {
     const search = {
-      bling: 'blaww',
-      burr: 'burr',
-      fizz: 'buzz',
-      foo: 'bar'
+      bling: "blaww",
+      burr: "burr",
+      fizz: "buzz",
+      foo: "bar",
     };
-    const queryParams = '?burr=burr&fizz=buzz&bling=blaww&foo=bar';
+    const queryParams = "?burr=burr&fizz=buzz&bling=blaww&foo=bar";
     // Expect an exact match.
     expect(matchSearch(search, undefined, true, queryParams)).toEqual({
       isExact: true,
       search,
-      searchKeyset: []
+      searchKeyset: [],
     });
     // Expect an exact match even though not specifying exact.
     expect(matchSearch(search, undefined, false, queryParams)).toEqual({
       isExact: true,
       search,
-      searchKeyset: []
+      searchKeyset: [],
     });
 
     const queryParamsTwo =
-      '?camp=flog_gnaw&burr=burr&fizz=buzz&bling=blaww&foo=bar&golf=wang';
+      "?camp=flog_gnaw&burr=burr&fizz=buzz&bling=blaww&foo=bar&golf=wang";
     // Expect a non-exact match to pass
     expect(matchSearch(search, undefined, false, queryParamsTwo)).toEqual({
       isExact: false,
       search: {
         ...search,
-        camp: 'flog_gnaw',
-        golf: 'wang'
+        camp: "flog_gnaw",
+        golf: "wang",
       },
-      searchKeyset: []
+      searchKeyset: [],
     });
     // Expect an exact match to fail
     expect(matchSearch(search, undefined, true, queryParamsTwo)).toEqual(null);
 
-    const queryParamsThree = '?record_label=brainfeeder&artist=teebs';
+    const queryParamsThree = "?record_label=brainfeeder&artist=teebs";
     // Expect a non-exact match to fail when the query params are missing expected keys.
     expect(matchSearch(search, undefined, false, queryParamsThree)).toEqual(
       null
@@ -81,50 +81,50 @@ describe('matchSearch', () => {
     );
   });
 
-  it('should calculate the correct match against the supplied search array', () => {
-    const queryParams = '?foo=bar&bar=buzz';
+  it("should calculate the correct match against the supplied search array", () => {
+    const queryParams = "?foo=bar&bar=buzz";
     // Expect an exact match to pass when all the keys are there.
-    expect(matchSearch(undefined, ['foo', 'bar'], true, queryParams)).toEqual({
+    expect(matchSearch(undefined, ["foo", "bar"], true, queryParams)).toEqual({
       isExact: true,
       search: {
-        bar: 'buzz',
-        foo: 'bar'
+        bar: "buzz",
+        foo: "bar",
       },
-      searchKeyset: ['foo', 'bar']
+      searchKeyset: ["foo", "bar"],
     });
 
     // Expect an exact match to pass even with duplicates.
     expect(
-      matchSearch(undefined, ['foo', 'bar', 'foo', 'bar'], true, queryParams)
+      matchSearch(undefined, ["foo", "bar", "foo", "bar"], true, queryParams)
     ).toEqual({
       isExact: true,
       search: {
-        bar: 'buzz',
-        foo: 'bar'
+        bar: "buzz",
+        foo: "bar",
       },
-      searchKeyset: ['foo', 'bar', 'foo', 'bar']
+      searchKeyset: ["foo", "bar", "foo", "bar"],
     });
 
     // Expect a an exact match to fail when there are more keys than expected.
-    expect(matchSearch(undefined, ['foo'], true, queryParams)).toEqual(null);
+    expect(matchSearch(undefined, ["foo"], true, queryParams)).toEqual(null);
 
     // Expect a non-exact match to pass when the supplied keys (and more) are there.
-    expect(matchSearch(undefined, ['bar'], false, queryParams)).toEqual({
+    expect(matchSearch(undefined, ["bar"], false, queryParams)).toEqual({
       isExact: false,
       search: {
-        bar: 'buzz',
-        foo: 'bar'
+        bar: "buzz",
+        foo: "bar",
       },
-      searchKeyset: ['bar']
+      searchKeyset: ["bar"],
     });
 
     // Expect a non-exact match to fail when the supplied keys (and more) are not there.
     expect(
-      matchSearch(undefined, ['bar', 'bling', 'blaww'], false, queryParams)
+      matchSearch(undefined, ["bar", "bling", "blaww"], false, queryParams)
     ).toEqual(null);
     // Expect an exact match to fail when the supplied keys (and more) are not there.
     expect(
-      matchSearch(undefined, ['bar', 'bling', 'blaww'], true, queryParams)
+      matchSearch(undefined, ["bar", "bling", "blaww"], true, queryParams)
     ).toEqual(null);
   });
 });
